a9105b4ed6521bab9ccb7d0c55bc0d74a9f1e0c7
Rajeev Siewnath <info@rajeevsiewnath.com>
2020-12-25 22:11:47 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker build -t rajeevsiewnath/multi-client:latest -t rajeevsiewnath/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rajeevsiewnath/multi-server:latest -t rajeevsiewnath/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rajeevsiewnath/multi-worker:latest -t rajeevsiewnath/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push rajeevsiewnath/multi-client:latest
+docker push rajeevsiewnath/multi-server:latest
+docker push rajeevsiewnath/multi-worker:latest
+docker push rajeevsiewnath/multi-client:$SHA
+docker push rajeevsiewnath/multi-server:$SHA
+docker push rajeevsiewnath/multi-worker:$SHA
+
+kubectl apply -f ./k8s
+
+kubectl set image deployments/client-deployments client=rajeevsiewnath/multi-client:$SHA
+kubectl set image deployments/server-deployments server=rajeevsiewnath/multi-server:$SHA
+kubectl set image deployments/worker-deployments worker=rajeevsiewnath/multi-worker:$SHA
